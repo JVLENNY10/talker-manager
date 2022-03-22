7ee0f97ac3f577330f8d8605b6b4006090e0ea90
João Victor <joao.lenny100@gmail.com>
2022-03-22 15:01:13 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -36,3 +36,46 @@ app.get('/talker/:id', (req, res) => {
       return res.status(200).json(result);
     });
 });
+
+// Requisito 3
+
+// Desenvolvi minha solução com informações desses sites:
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
+
+// Lembrei do toString por conta desse site:
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
+const generateToken = () => {
+  const token1 = Math.round(Math.random() * 999999999999).toString(36);
+  const token2 = Math.round(Math.random() * 999999999999).toString(36);
+  const finalToken = token1 + token2;
+
+  if (finalToken.length === 16) return finalToken;
+
+  return generateToken();
+};
+
+const checkEmail = (email, res) => {
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } if (!email.includes('@') || !email.includes('.com')) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+};
+
+const checkPassword = (password, res) => {
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+};
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  checkEmail(email, res);
+  checkPassword(password, res);
+  res.status(200).json({ token: generateToken() });
+});
